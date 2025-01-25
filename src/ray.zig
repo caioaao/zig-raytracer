361c47e04770a4ec87dfa300881ef816e0b461ad
Caio Oliveira <caioaao@gmail.com>
2025-01-24 21:20:48 -0300

    6. visualize sphere's normal map

--- src/ray.zig
+++ src/ray.zig
@@ -1,8 +1,7 @@
+const std = @import("std");
 const Vec3 = @import("./vec3.zig").Vec3;
 const Point3 = @import("./vec3.zig").Point3;
 const RGB = @import("./color.zig").RGB;
-
-const RED = RGB.newFromRatios(1, 0, 0);
 
 pub const Ray = struct {
     origin: Point3,
@@ -21,9 +20,16 @@ pub const Ray = struct {
     }
 
     pub fn color(self: Ray) RGB {
-        if (self.hitSphere(.{ .x = 0, .y = 0, .z = -1 }, 0.5)) {
-            return RED;
+        const t = self.hitSphere(.{ .x = 0, .y = 0, .z = -1 }, 0.5);
+        if (t > 0.0) {
+            const normal = self.at(t).sub(.{ .x = 0, .y = 0, .z = -1 }).normalize();
+            return RGB.newFromRatios(
+                (normal.x + 1) * 0.5,
+                (normal.y + 1) * 0.5,
+                (normal.z + 1) * 0.5,
+            );
         }
+
         const a = self.direction.y * 0.5 + 0.5;
         return RGB.newFromRatios(
             (1.0 - a) + a * 0.5,
@@ -32,13 +38,15 @@ pub const Ray = struct {
         );
     }
 
-    pub fn hitSphere(self: Ray, center: Point3, radius: f64) bool {
+    pub fn hitSphere(self: Ray, center: Point3, radius: f64) f64 {
         const oc = center.sub(self.origin);
         const a = Vec3.dot(self.direction, self.direction);
         const b = -2.0 * Vec3.dot(self.direction, oc);
         const c = Vec3.dot(oc, oc) - radius * radius;
         const discriminant = b * b - 4 * a * c;
-        return discriminant >= 0;
+
+        if (discriminant < 0) return -1.0;
+        return (-b - std.math.sqrt(discriminant)) / (2.0 * a);
     }
 };
 
